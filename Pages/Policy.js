8a56546f584527b71b3d9b35f44df2a535e7c4fd
Data2Money <suunya43@gmail.com>
2025-04-03 11:44:05 +0800

    Create Policy.js

---
+++ Pages/Policy.js
@@ -0,0 +1,8 @@
+export default function Policy() {
+  return (
+    <div>
+      <h1>Disclaimer & Privacy Policy</h1>
+      <p>This website provides general information only. Trading involves risk.</p>
+    </div>
+  );
+}
