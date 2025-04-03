7a72e54e817ec7943337ca78fb8243c495878b5a
Data2Money <suunya43@gmail.com>
2025-04-03 11:56:36 +0800

    Create strategy.js

---
+++ pages/strategy.js
@@ -0,0 +1,8 @@
+export default function Strategy() {
+  return (
+    <div>
+      <h1>Strategy Performance</h1>
+      <p>Details about AI + Order Flow trading logic coming soon.</p>
+    </div>
+  );
+}
