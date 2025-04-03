9b3456171874d69f71e7c030496730cbf5a4df4b
Data2Money <suunya43@gmail.com>
2025-04-03 11:52:55 +0800

    Delete Pages directory

--- Pages/Macro.js
+++
@@ -1,8 +0,0 @@
-export default function Macro() {
-  return (
-    <div>
-      <h1>Macroeconomic Dashboard</h1>
-      <p>Global interest rates, inflation, and GDP will be integrated here.</p>
-    </div>
-  );
-}
--- Pages/Policy.js
+++
@@ -1,8 +0,0 @@
-export default function Policy() {
-  return (
-    <div>
-      <h1>Disclaimer & Privacy Policy</h1>
-      <p>This website provides general information only. Trading involves risk.</p>
-    </div>
-  );
-}
