39784ed4199a8c991af83bd0d4d493635d583f47
Data2Money <suunya43@gmail.com>
2025-04-03 11:57:44 +0800

    Create macro.js

---
+++ pages/macro.js
@@ -0,0 +1,8 @@
+export default function Macro() {
+  return (
+    <div>
+      <h1>Macroeconomic Dashboard</h1>
+      <p>Global interest rates, inflation, and GDP will be integrated here.</p>
+    </div>
+  );
+}
